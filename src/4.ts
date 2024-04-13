1b530dcc319fb47ed56989d421d7d3f6ec5ccec6
maxmelnyk1311 <141562499+maxmelnyk1311@users.noreply.github.com>
2024-04-14 01:39:58 +0300

    version 1.0

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+    private signature: number = Math.random();
+    getSignature(): number { 
+        return this.signature; 
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    comeIn(person: Person) {
+      if (this.door) {
+        this.tenants.push(person);
+      }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key) {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        // console.log("Wellcome!");
+      } else {
+        // console.log("Where is key?");
+      }
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    getKey(): Key { return this.key }
+  }
 const key = new Key();
 
 const house = new MyHouse(key);
